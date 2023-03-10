57c8bc3d24c5eb8f9e139cb9bd1b8b04c011d7f9
Andrew Tansinsin <andrew.tansinsin@ph.softvision.global>
2023-03-10 15:44:44 +0800

    Commit initial code for assignment

--- Session2Part1Assignment/HttpClientTests.cs
+++ Session2Part1Assignment/HttpClientTests.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 [assembly: Parallelize(Workers = 10, Scope = ExecutionScope.MethodLevel)]
 namespace Session2Part1Assignment
@@ -18,13 +19,15 @@ namespace Session2Part1Assignment
 
         private static readonly string BaseURL = "https://petstore.swagger.io/v2/";
 
-        private static readonly string UsersEndpoint = "user";
+        private static readonly string UsersEndpoint = "pet";
 
         private static string GetURL(string enpoint) => $"{BaseURL}{enpoint}";
 
         private static Uri GetURI(string endpoint) => new Uri(GetURL(endpoint));
 
-        private readonly List<UserModel> cleanUpList = new List<UserModel>();
+        private readonly List<PetModel> cleanUpList = new List<PetModel>();
+
+        private static readonly string V = "https://images.app.goo.gl/KvPjfUZwUKfeJ34M8";
 
         [TestInitialize]
         public void TestInitialize()
@@ -37,145 +40,34 @@ namespace Session2Part1Assignment
         {
            foreach (var data in cleanUpList)
             {
-                var httpResponse = await httpClient.DeleteAsync(GetURL($"{UsersEndpoint}/{data.Username}"));
+                var httpResponse = await httpClient.DeleteAsync(GetURL($"{UsersEndpoint}/{data.Name}"));
             }
         }
-
-        [TestMethod]
-        public async Task GetMethod()
-        {
-            #region create data
-
-            // Create Json Object
-            UserModel userData = new UserModel()
-            {
-                Username = "test123.get",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
-            };
-
-            // Serialize Content
-            var request = JsonConvert.SerializeObject(userData);
-            var postRequest = new StringContent(request, Encoding.UTF8, "application/json");
-
-            // Send Post Request
-            await httpClient.PostAsync(GetURL(UsersEndpoint), postRequest);
-
-            #endregion
-
-            #region get data
-
-            // Send Request
-            var httpResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
-
-            // Get Content
-            var httpResponseMessage = httpResponse.Content;
-            
-            // Get Status Code
-            var statusCode = httpResponse.StatusCode;
-
-            // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(httpResponseMessage.ReadAsStringAsync().Result);
-
-            #endregion
-
-            #region cleanupdata
-
-            // Add data to cleanup list
-            cleanUpList.Add(listUserData);
-
-            #endregion
-
-            #region assertion
-
-            // Assertion
-            Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 200");
-            Assert.IsTrue(listUserData.Username == userData.Username);
-
-            #endregion
-        }
-
-        [TestMethod]
-        public async Task PostMethod()
-        {
-            #region create data and send post request
-
-            // Create Json Object
-            UserModel userData = new UserModel()
-            {
-                Username = "test123.post",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
-            };
-
-            // Serialize Content
-            var request = JsonConvert.SerializeObject(userData);
-            var postRequest = new StringContent(request,Encoding.UTF8, "application/json");
-
-            // Send Request
-            var httpResponse = await httpClient.PostAsync(GetURL(UsersEndpoint), postRequest);
-
-            // Get Status Code
-            var statusCode = httpResponse.StatusCode;
-
-            #endregion
-
-            #region get created data
-
-            // Get Request
-            var getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
-
-            // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
-
-            var createdUserData = listUserData.Username;
-
-            #endregion
-
-            #region cleanupdata
-
-            // Add data to cleanup list
-            cleanUpList.Add(listUserData);
-            #endregion
-
-            #region assertion
-
-            // Assertion
-            Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 201");
-            Assert.AreEqual(userData.Username, createdUserData, "Username not matching");
-            
-            #endregion
-
-        }
-
 
         [TestMethod]
         public async Task PutMethod()
         {
             #region create data
 
-            // Create Json Object
-            UserModel userData = new UserModel()
+            // Create Json Object here
+            PetModel petData = new PetModel()
             {
-                Username = "test123.put",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
+                Id = 88,
+                Category = {
+                    Id = 8008,
+                    Name = "Persian Cat"
+                },
+                Name = "Garfield",
+                PhotoUrls = new string[] {V},
+                Tags = {
+                    Id = new long[] {2023},
+                    Name = new string[] {"Andrew Tansinsin"}
+                },
+                Status = "available"
             };
 
             // Serialize Content
-            var request = JsonConvert.SerializeObject(userData);
+            var request = JsonConvert.SerializeObject(petData);
             var postRequest = new StringContent(request, Encoding.UTF8, "application/json");
 
             // Send Post Request
@@ -186,37 +78,41 @@ namespace Session2Part1Assignment
             #region get Username of the created data
 
             // Get Request
-            var getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
+            var getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{petData.Name}"));
 
             // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
+            var listPetData = JsonConvert.DeserializeObject<PetModel>(getResponse.Content.ReadAsStringAsync().Result);
 
             // filter created data
-            var createdUserData = listUserData.Username;
+            var createdPetData = listPetData.Name;
 
             #endregion
 
             #region send put request to update data
 
-            // Update value of userData
-            userData = new UserModel()
+            // Update value of petData
+            petData = new PetModel()
             {
-                Id = listUserData.Id,
-                Username = "test123.put.updated",
-                FirstName = listUserData.FirstName,
-                LastName = listUserData.LastName,
-                Email = listUserData.Email,
-                Password = listUserData.Password,
-                Phone = listUserData.Phone,
-                UserStatus = listUserData.UserStatus,
+                Id = listPetData.Id,
+                Category = {
+                    Id = listPetData.Category.Id,
+                    Name = listPetData.Category.Name
+                },
+                Name = "Garfunkel.put.updated",
+                PhotoUrls = new string[] { V },
+                Tags = {
+                    Id = listPetData.Tags.Id,
+                    Name = listPetData.Tags.Name
+                },
+                Status = listPetData.Status
             };
             
             // Serialize Content
-            request = JsonConvert.SerializeObject(userData);
+            request = JsonConvert.SerializeObject(petData);
             postRequest = new StringContent(request, Encoding.UTF8, "application/json");
 
             // Send Put Request
-            var httpResponse = await httpClient.PutAsync(GetURL($"{UsersEndpoint}/{createdUserData}"), postRequest);
+            var httpResponse = await httpClient.PutAsync(GetURL($"{UsersEndpoint}/{createdPetData}"), postRequest);
 
             // Get Status Code
             var statusCode = httpResponse.StatusCode;
@@ -226,20 +122,20 @@ namespace Session2Part1Assignment
             #region get updated data
 
             // Get Request
-            getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
+            getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{petData.Name}"));
 
             // Deserialize Content
-            listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
+            listPetData = JsonConvert.DeserializeObject<PetModel>(getResponse.Content.ReadAsStringAsync().Result);
 
             // filter created data
-            createdUserData = listUserData.Username;
+            createdPetData = listPetData.Name;
 
             #endregion
 
             #region cleanup data
 
             // Add data to cleanup list
-            cleanUpList.Add(listUserData);
+            cleanUpList.Add(listPetData);
 
             #endregion
 
@@ -247,222 +143,10 @@ namespace Session2Part1Assignment
 
             // Assertion
             Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 201");
-            Assert.AreEqual(userData.Username, createdUserData, "Username not matching");
+            Assert.AreEqual(petData.Name, createdPetData, "Username not matching");
 
             #endregion
             
         }
-
-        [TestMethod]
-        public async Task DeleteMethod()
-        {
-            #region create data
-
-            // Create Json Object
-            UserModel userData = new UserModel()
-            {
-                Username = "test123.delete",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
-            };
-
-            // Serialize Content
-            var request = JsonConvert.SerializeObject(userData);
-            var postRequest = new StringContent(request, Encoding.UTF8, "application/json");
-
-            // Send Post Request
-            await httpClient.PostAsync(GetURL(UsersEndpoint), postRequest);
-
-            #endregion
-
-            #region get Username of the created data
-
-            // Get Request
-            var getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
-
-            // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
-
-            // filter created data
-            var createdUserData = listUserData.Username;
-
-            #endregion
-
-            #region send delete request
-
-            // Send Delete Request
-            var httpResponse = await httpClient.DeleteAsync(GetURL($"{UsersEndpoint}/{createdUserData}"));
-
-            // Get Status Code
-            var statusCode = httpResponse.StatusCode;
-
-            #endregion
-
-            #region assertion
-
-            // Assertion
-            Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 201");
-
-            #endregion
-        }
-
-        [TestMethod]
-        public async Task SendPostGetMethod()
-        {
-            #region create data
-
-            // Create Json Object
-            UserModel userData = new UserModel()
-            {
-                Username = "test123.sendpostget",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
-            };
-
-            #endregion
-
-            #region send post request
-
-            var httpResponse = await SendAsyncFunction(HttpMethod.Post, UsersEndpoint, userData);
-
-            var statusCode = httpResponse.StatusCode;
-
-            #endregion
-
-            #region get created data
-
-            // Get Request
-            var getResponse = await SendAsyncFunction(HttpMethod.Get, $"{UsersEndpoint}/{userData.Username}");
-
-            // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
-
-            var createdUserData = listUserData.Username;
-
-            #endregion
-
-            #region cleanupdata
-
-            // Add data to cleanup list
-            cleanUpList.Add(listUserData);
-            #endregion
-
-            #region assertion
-
-            // Assertion
-            Assert.AreEqual(HttpStatusCode.OK, statusCode, "Status code is not equal to 201");
-            Assert.AreEqual(userData.Username, createdUserData, "Username not matching");
-
-            #endregion
-
-        }
-
-        /// <summary>
-        /// Reusable method
-        /// </summary>
-        private async Task<HttpResponseMessage> SendAsyncFunction(HttpMethod method, string url, UserModel userData = null)
-        {
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
-
-            httpRequestMessage.Method = method;
-            httpRequestMessage.RequestUri = GetURI(url);
-            httpRequestMessage.Headers.Add("Accept", "application/json");
-
-            if (userData != null)
-            {
-                // Serialize Content
-                var request = JsonConvert.SerializeObject(userData);
-                httpRequestMessage.Content = new StringContent(request, Encoding.UTF8, "application/json");
-            } 
-
-            var httpResponse = await httpClient.SendAsync(httpRequestMessage);
-
-            return httpResponse;
-        }
-
-
-        [TestMethod]
-        public async Task PatchMethod()
-        {
-            #region create data
-
-            // Create Json Object
-            UserModel userData = new UserModel()
-            {
-                Username = "test123.patch",
-                FirstName = "testFName",
-                LastName = "testLName",
-                Email = "test@email.com",
-                Password = "123",
-                Phone = "123456789",
-                UserStatus = 0
-            };
-
-            // Serialize Content
-            var request = JsonConvert.SerializeObject(userData);
-            var postRequest = new StringContent(request, Encoding.UTF8, "application/json");
-
-            // Send Post Request
-            await httpClient.PostAsync(GetURL(UsersEndpoint), postRequest);
-
-            #endregion
-
-            #region get Username of the created data
-
-            // Get Request
-            var getResponse = await httpClient.GetAsync(GetURI($"{UsersEndpoint}/{userData.Username}"));
-
-            // Deserialize Content
-            var listUserData = JsonConvert.DeserializeObject<UserModel>(getResponse.Content.ReadAsStringAsync().Result);
-
-            // filter created data
-            var createdUserData = listUserData.Username;
-
-            #endregion
-
-            #region send patch request to update data
-
-            // Update value of userData
-            userData = new UserModel()
-            {
-                Username = "test123.patch.updated"
-            };
-
-            // Serialize Content
-            request = JsonConvert.SerializeObject(userData);
-            postRequest = new StringContent(request, Encoding.UTF8, "application/json");
-
-            // Send Put Request
-            var httpResponse = await httpClient.PatchAsync(GetURL($"{UsersEndpoint}/{createdUserData}"), postRequest);
-
-            // Get Status Code
-            var statusCode = httpResponse.StatusCode;
-
-            #endregion
-
-            #region cleanup data
-
-            // Add data to cleanup list
-            cleanUpList.Add(listUserData);
-
-            #endregion
-
-            #region assertion
-
-            // Assertion
-            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, statusCode, "Status code is not equal to 405");
-
-            #endregion
-
-        }
-
     }
 }
---
+++ Session2Part1Assignment/PetModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session2Part1Assignment
+{
+    public class PetModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("category")]
+        public Category Category { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("photoUrls")]
+        public string[] PhotoUrls { get; set; }
+
+        [JsonProperty("tags")]
+        public Tags Tags { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Tags
+    {
+        [JsonProperty("id")]
+        public long[] Id { get; set; }
+
+        [JsonProperty("name")]
+        public string[] Name { get; set; }
+    }
+
+}
